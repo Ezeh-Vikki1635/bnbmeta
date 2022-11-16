af06b9c76e79302c03a5bcde4b1df3070403c995
Ezeh-Vikki1635 <103960935+Ezeh-Vikki1635@users.noreply.github.com>
2022-11-16 11:14:42 +0100

    Update Place.js
    Update code

--- src/pages/Place/Place.js
+++ src/pages/Place/Place.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-
+import PlaceList from "./components/PlaceList/PlaceList";
 import PlaceNav from "./components/PlaceNav/PlaceNav";
 
 
@@ -12,7 +12,7 @@ const Place = () => {
     <Fragment>
       < Header />
       < PlaceNav />
-      
+      < PlaceList />
       < Footer />
       
     </Fragment>
