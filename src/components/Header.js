abeeea0d9cf33facd5e11a9ad59f2a37a270484c
Ezeh-Vikki1635 <103960935+Ezeh-Vikki1635@users.noreply.github.com>
2022-11-16 10:39:26 +0100

    Update Header.js
    Update code

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,7 @@ const Header = () => {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/Place-to-stay You">Place to stay</Link>
+              <Link to="/Place-to-stay">Place to stay</Link>
             </li>
             <li>
               <Link to="/">NFTs</Link>
